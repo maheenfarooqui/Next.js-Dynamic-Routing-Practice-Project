27f0942f22367a2abc2079033cb457f27801a4bd
maheen <maheenzuhra@gmail.com>
2025-10-31 17:22:01 +0500

    make product card

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
+import Image from "next/image"; 
 export default function Products() {
   const ourProducts = [
     {
       name: "Perfume",
       slug: "perfume",
+      price:"$49.99",
+      image: "/perfume.jpg",
     },
     {
       name: "Hand Bag",
       slug: "handbag",
+      price: "$89.99",
+      image: "/handbagBlack.jpg",
     },
     {
       name: "Watch",
       slug: "watch",
+      price: "$129.99",
+      image: "/watch.jpg",
     },
   ];
   return (
@@ -39,14 +46,36 @@ export default function Products() {
           marginTop: "40px",
         }}
       >
-         <ul style={{ listStyle: "none", padding: 0 }}>
+         <div style={{display:"flex", gap: "30px"}}>
         {ourProducts.map((item) => (
-          <li key={item.slug}>
-           <Link href={`/products/${item.slug}`}>{item.name}</Link>
+          <div key={item.slug}
+          style={{
+              border: "2px solid #ddd",
+              borderRadius: "15px",
+              padding: "20px",
+              width: "220px",
+              textAlign: "center",
+              boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
+              backgroundColor: "#fff",
+            }}>
+               <Image
+              src={item.image}
+              alt={item.name}
+              width={200}
+              height={200}
+              style={{
+                borderRadius: "10px",
+                objectFit: "cover",
+                marginBottom: "15px",
+              }}
+            />
+            <h2 style={{ color: "darkgreen", fontSize: "20px" }}>{item.name}</h2>
+            <p style={{ color: "#555", margin: "10px 0" }}>{item.price}</p>
+           <Link href={`/products/${item.slug}`}>Buy Now</Link>
 
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
       </div>
     </div>
   );
